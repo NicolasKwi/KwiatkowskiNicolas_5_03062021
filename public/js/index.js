6ccd099453ef93cae05e84224cf9b272f0bce087
NicolasKwi <nicolas.kwi@gmail.com>
2021-06-07 17:03:16 +0200

    V0 - Mise en place

---
+++ public/js/index.js
@@ -0,0 +1,55 @@
+// recupere l'element où sera affiché les cards
+const listProduit = document.querySelector(".cont-list");
+
+const getProductsApi = async () => {
+  fetch("http://localhost:3000/api/cameras")
+    .then(function (res) {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then(function (data) {
+      // Ajoute un card pour chaques elements de data
+      data.forEach((element) => {
+        afficheListeCamera(element);
+      });
+    })
+    .catch(function (err) {
+      // Une erreur est survenue
+      alert(err);
+    });
+};
+
+const afficheListeCamera = (camera) => {
+  if (camera != undefined) {
+    // Ajoute la card du produit dans la liste des produits
+    listProduit.innerHTML += `<a href="./pages/produit.html?${camera._id}" class="card col-9 col-md-4 col-lg-3 m-2 shadow text-decoration-none text-dark">
+                                <img src="${
+                                  camera.imageUrl
+                                }" alt="" class="card-img-top" />
+                                <div class="card-body">
+                                  <h3 class="card-title text-center text-primary">${
+                                    camera.name
+                                  }</h3>                                 
+                                  <h6 class="card-subtitle mb-2 text-muted">
+                                    ${
+                                      camera.lenses.length > 1
+                                        ? camera.lenses.length +
+                                          " choix de lentilles"
+                                        : "Lentille de " + camera.lenses[0]
+                                    }
+                                  </h6>
+                                   <h5 class="card-title mb-3 text-black-50">
+                                   Prix : ${(camera.price/100).toString().replace('.',',')} €
+                                  </h5>
+                                  <p class="card-text truncate-overflow mx-3">${
+                                    camera.description
+                                  }</p>                
+                                </div>
+                              </a>`;
+  }
+};
+window.addEventListener("DOMContentLoaded", () => {
+  // Une fois que la page est chargé, on recupère la liste des produits sur le serveur
+  getProductsApi();
+});
